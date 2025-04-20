0742aa70b311573fa20b851e32971d75ecddd9d9
Asmaa Alaghbari <73389316+Asmaa-Alaghbari@users.noreply.github.com>
2025-04-20 19:12:40 +0300

    Rock Paper Scissor - Modularizing Code

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,30 +3,48 @@ import random
 rock_paper_scissors = ("r", "p", "s")
 rock_paper_scissors_icons = {"r": "🪨", "p": "📄", "s": "✂️"}
 
-while True:
-    user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
-    computer_choice = random.choice(rock_paper_scissors)
 
-    if user_choice in rock_paper_scissors:
-        print(f"You chose {rock_paper_scissors_icons[user_choice]}")
-        print(f"Computer chose {rock_paper_scissors_icons[computer_choice]}")
-
-        if (
-            (user_choice == "r" and computer_choice == "s")
-            or (user_choice == "p" and computer_choice == "r")
-            or (user_choice == "s" and computer_choice == "p")
-        ):
-            print("You win! 🥳")
-
-        elif user_choice == computer_choice:
-            print("It's a tie!")
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
+        if user_choice in rock_paper_scissors:
+            return user_choice
         else:
-            print("Computer win! 🥲")
+            print("Invalid choice! Try again!")
+
+
+def display_chioces(user_choice, computer_choice):
+    print(f"You chose {rock_paper_scissors_icons[user_choice]}")
+    print(f"Computer chose {rock_paper_scissors_icons[computer_choice]}")
+
+
+def determine_winner(user_choice, computer_choice):
+    if (
+        (user_choice == "r" and computer_choice == "s")
+        or (user_choice == "p" and computer_choice == "r")
+        or (user_choice == "s" and computer_choice == "p")
+    ):
+        print("You win! 🥳")
+
+    elif user_choice == computer_choice:
+        print("It's a tie!")
     else:
-        print("Invalid choice! Try again!")
-        continue
+        print("Computer win! 🥲")
 
-    replay = input("Continue? (y/n): ").lower()
 
-    if replay == "n":
-        break
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = random.choice(rock_paper_scissors)
+
+        get_user_choice()
+        display_chioces(user_choice, computer_choice)
+        determine_winner(user_choice, computer_choice)
+
+        replay = input("Continue? (y/n): ").lower()
+
+        if replay == "n":
+            break
+
+
+play_game()
